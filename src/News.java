40c4239e84cb778b4efc5a120af1385215b6c0e8
XIANG-LEE <71392262+Jormungand-Stark@users.noreply.github.com>
2022-02-21 20:26:45 +0800

    新闻类

---
+++ src/News.java
@@ -0,0 +1,22 @@
+package com.example.activitytest.CustomType;
+
+public class News {
+    private String title; // 标题栏
+    private String content; // 内容
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
